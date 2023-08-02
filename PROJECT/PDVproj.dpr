ad59eac9f1177e1ea1d6b9b984648f4ed8d4b264
Murilo57 <murilo.ti32@outlook.com>
2023-08-02 15:35:01 -0300

    Primeiro Commit 1.0
    Esboço da parte visual

---
+++ PROJECT/PDVproj.dpr
@@ -0,0 +1,13 @@
+program PDVproj;
+
+uses
+  Forms,
+  PDV in '..\FORM\PDV.pas' {frmPDV};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmPDV, frmPDV);
+  Application.Run;
+end.
